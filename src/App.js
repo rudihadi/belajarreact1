21136e286f1857cfb6a6735a69f0a824749dcb33
rudihadi <rudihadi5395@gmail.com>
2022-01-20 16:15:03 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="childleft">
+
+      </div>
+      <div className="childright">
+        <div className="form">
+          <div>Signs in to INDICO </div>
+          <lable className="textform">Email</lable>
+          <div>
+            <input type="text" placeholder="Email" className="inputemail"/>
+          </div>
+          <lable className="textform">Password</lable>
+          <div>
+            <input type="text" placeholder="Password" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
